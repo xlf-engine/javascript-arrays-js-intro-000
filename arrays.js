98c302ceff2d2ae9d8d5725d7bf62425d1d119dc
NICOLAS T <nicolasturek@gmail.com>
2020-03-31 02:06:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.slice(0);
+  array.slice(0);
   return array;
 }
 
