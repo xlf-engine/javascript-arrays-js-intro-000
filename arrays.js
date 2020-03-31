eb5a720744e10d679c625345d1cb6ee02cc21eee
NICOLAS T <nicolasturek@gmail.com>
2020-03-31 01:39:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,11 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array;
 }
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+
+}
